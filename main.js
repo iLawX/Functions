6d281c165dca7b7bebd0a98c563282bab2e30fce
Munthader <65040318+iLawX@users.noreply.github.com>
2024-08-16 18:58:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,309 @@
+const functions = [
+  {
+    title: "Paraphrasing",
+    unit: "Unit 1",
+    phrases: [
+      {
+        phrase:
+          "The report <span>says that</span> oil reserves will last until 2038.",
+      },
+      { phrase: "<span>In other words</span>, we need to plan for different" },
+      {
+        phrase: "<span>From what I understand</span>, oil reserves are limited",
+      },
+      {
+        phrase: "<span>Basically</span>, this means we can't rely only on oil",
+      },
+      {
+        phrase:
+          "<span>Let's put it this way</span>, we have to find alternative engery sources",
+      },
+      {
+        phrase: "<span>You mean</span> our oil won't last forever?",
+      },
+      {
+        phrase:
+          "<span>What you're saying is</span> we can't ignore the future lack of oil.",
+      },
+      {
+        phrase:
+          "<span>Just to be sure i've got this</span>, after 2038, we won't have any oil left?",
+      },
+      {
+        phrase:
+          "<span>As I understand it</span>, the future of oil is limited.",
+      },
+      {
+        phrase:
+          "<span>I just want to clarify that</span> oil is limited and won't last forever.",
+      },
+    ],
+  },
+  {
+    title: "Assigning tasks",
+    unit: "Unit 1",
+    phrases: [
+      {
+        phrase:
+          "<span>Could you please</span> complete the report's conclusion?",
+      },
+      {
+        phrase:
+          "<span>We'd appreciate your help with</span> the conclusion of the report",
+      },
+      { phrase: "<span>I'd like you to</span> finish the report for the team" },
+      { phrase: "<span>Would you please</span> finish it by Monday lunchtime" },
+      {
+        phrase:
+          "<span>Would you mind</span> finishing the report as soon as possible?",
+      },
+    ],
+  },
+  {
+    title: "Talking about assumptions",
+    unit: "Unit 2",
+    phrases: [
+      { phrase: "<span>I assume (that)</span> everyone has read my email." },
+      {
+        phrase:
+          "<span>Assuming (that)</span> the boss approves my request, I will leave tomorrow.",
+      },
+      {
+        phrase:
+          "<span>I take it for granted (that)</span> there will be a few problems.",
+      },
+      { phrase: "<span>I suppose (that)</span> we will need to take notes." },
+      {
+        phrase:
+          "<span>Don't take</span> Ahmed's cooperation for <span>for granted</span>.",
+      },
+      {
+        phrase:
+          "<span>I think it's a safe assumption (that)</span> to say that we are going to work overtime.",
+      },
+      { phrase: "I think that's <span>a safe bet</span>." },
+      {
+        phrase:
+          "I wouldn't <span>take it for granted (that)</span> all the participants will speak English",
+      },
+    ],
+  },
+  {
+    title: "Making analogies",
+    unit: "Unit 2",
+    phrases: [
+      { phrase: "A smart well <span>is like</span> a tree." },
+      { phrase: "it's <span>as</span> smooth <span>as</span> silk." },
+      { phrase: "This scuba suit <span>is similar to</span> a shark's skin." },
+    ],
+  },
+  {
+    title: "Interpreting graphs and charts",
+    unit: "Unit 3",
+    phrases: [
+      { phrase: "<span>What does this mean?</span>" },
+      { phrase: "<span>This means that</span> oil prices rose in June." },
+      { phrase: "<span>What does this suggest</span>." },
+      {
+        phrase:
+          "<span>This suggests that</span> more paper should be ordered now.",
+      },
+      { phrase: "<span>What does this show?</span>" },
+      {
+        phrase:
+          "<span>This shows that</span> white cars are more popular than black cars.",
+      },
+      {
+        phrase:
+          "<span>What this doesn't mean / suggest / show is that</span> women buy more detergent than men.",
+      },
+      {
+        phrase:
+          "<span>The trend is</span> upward, increasing demand every summer.",
+      },
+    ],
+  },
+  {
+    title: "Dividing and classifying",
+    unit: "Unit 3",
+    phrases: [
+      {
+        phrase: "Eggs are <span>classified according to</span> size and color.",
+      },
+      { phrase: "Books are <span>categorized by</span> subject." },
+      { phrase: "Fish are <span>sorted by</span> weight." },
+      { phrase: "Fires are <span>grouped by</span> fuel source." },
+      { phrase: "Chickens are <span>divided by</span> gender." },
+      { phrase: "Airplane seats are <span>rated based on</span>price." },
+      { phrase: "The <span>criteria</span> are size and color" },
+      {
+        phrase:
+          "The <span>characteristics</span> they have <span>in common are</span> size and color.",
+      },
+      { phrase: "It <span>meets / does not meet the criteria</span>." },
+    ],
+  },
+  {
+    title: "Querying",
+    unit: "Unit 4",
+    phrases: [
+      { phrase: "<span>Are you sure ... ?</span>" },
+      { phrase: "<span>OK, but why ... ?</span>" },
+      { phrase: "<span>I have a query about / regarding ...</span>" },
+      { phrase: "<span>Do you really ... ?</span>" },
+      { phrase: "<span>Is this really ... ?</span>" },
+    ],
+  },
+  {
+    title: "Expressing wants and needs",
+    unit: "Unit 4",
+    phrases: [
+      { phrase: "<span>We need ...</span>" },
+      { phrase: "<span>We must have ...</span>" },
+      { phrase: "<span>We have to ...</span>" },
+      { phrase: "<span>That's a must.</span>" },
+      { phrase: "<span>... is required.</span>" },
+      { phrase: "<span>We want ...</span>" },
+      { phrase: "<span>We would prefer ...</span>" },
+      { phrase: "<span>We could use ...</span>" },
+      { phrase: "<span>We could do with ...</span>" },
+      { phrase: "<span>It would be nice to have ...</span>" },
+      { phrase: "<span>... would be nice.</span>" },
+    ],
+  },
+  {
+    title: "Asking for and giving estimates",
+    unit: "Unit 5",
+    phrases: [
+      { phrase: "<span>Can you give me an estimate of the cost?</span>" },
+      { phrase: "<span>Can you give me a rough estimate of the cost? </span>" },
+      { phrase: "<span>Can you give me a rough idea of the cost?</span>" },
+      {
+        phrase: "<span>Can you give me an approximate idea of the cost?</span>",
+      },
+      { phrase: "<span>Can you give me some idea of the cost?</span>" },
+      {
+        phrase:
+          "<span>Can you give me an approximate figure for the cost?</span>",
+      },
+      {
+        phrase: "<span>Can you give me a ballpark figure for the cost?</span>",
+      },
+      {
+        phrase: "<span>Can you give me a projected figure for the cost?</span>",
+      },
+      { phrase: "<span>What's the estimated cost?</span>" },
+      { phrase: "<span>What's the projected cost?</span>" },
+      { phrase: "<span>What's the approximate cost?</span>" },
+      { phrase: "<span>Approximately how much will it cost?</span>" },
+      { phrase: "<span>Roughly how much will it cost?</span>" },
+      { phrase: "<span>About how much will it cost?</span>" },
+      { phrase: "<span>Approximately how many people will there be? </span>" },
+      { phrase: "<span>Roughly how many people will there be?</span>" },
+      { phrase: "<span>How much do you reckon it will cost?</span>" },
+      { phrase: "<span>How many people do you reckon there will be</span>" },
+    ],
+  },
+  {
+    title: "Expressing ambivalence",
+    unit: "Unit 5",
+    phrases: [
+      { phrase: "<span>I don't mind.</span>" },
+      { phrase: "<span>Anything will do.</span>" },
+      { phrase: "<span>Anything will be OK.</span>" },
+      { phrase: "<span>I don't mind what.</span>" },
+      { phrase: "<span>Whatever.</span>" },
+      { phrase: "<span>Wherever.</span>" },
+      { phrase: "<span>Whenever.</span>" },
+      { phrase: "<span>Whoever.</span>" },
+      { phrase: "<span>However.</span>" },
+      { phrase: "<span>Whichever.</span>" },
+      { phrase: "<span>I have mixed feelings about this issue.</span>" },
+      {
+        phrase:
+          "<span>On the one hand, it will have a positive impact, but on the other hand, it's very expensive.</span>",
+      },
+      {
+        phrase:
+          "<span>I'm ambivalent about assigning Ray to this project; it's up to you.</span>",
+      },
+      {
+        phrase:
+          "<span>I'm in two minds about assigning Ray to this project; it's your call. On the plus side, it will have a positive impact, but on the minus side, it's very expensive.</span>",
+      },
+      {
+        phrase:
+          "<span>On the positive side, it will have a positive impact, but on the negative side, it's very expensive.</span>",
+      },
+      {
+        phrase:
+          "<span>On the up side it, will have a positive impact, but on the down side, it's very expensive.</span>",
+      },
+    ],
+  },
+];
+
+let form = document.forms[0];
+let search = document.getElementById("search");
+let unit = document.getElementById("unit");
+let functionsDiv = document.querySelector(".functions");
+form.oninput = function () {
+  if (unit.value === "all") {
+    let filteredFunctions = functions.filter((e) =>
+      e.title.toLowerCase().startsWith(search.value.toLowerCase())
+    );
+    displayElementsFrom(filteredFunctions);
+  } else {
+    let filteredFunctions = functions.filter(
+      (e) =>
+        e.title.toLowerCase().startsWith(search.value.toLowerCase()) &&
+        e.unit === unit.value
+    );
+    displayElementsFrom(filteredFunctions);
+  }
+};
+
+function displayElementsFrom(from) {
+  functionsDiv.innerHTML = "";
+  from.forEach((f) => {
+    let func = document.createElement("div");
+    func.className = "function";
+    let funcTitleDiv = document.createElement("div");
+    funcTitleDiv.className = `title`;
+    let funcTitle = document.createElement("h2");
+    funcTitle.innerHTML = f.title;
+    let icon = document.createElement("i");
+    icon.className = "fa-solid fa-circle-chevron-down";
+    let phrases = document.createElement("div");
+    phrases.className = "phrases";
+    let phrasesContent = document.createElement("div");
+    phrasesContent.className = "phrases-content";
+    let list = document.createElement("ul");
+    f.phrases.forEach((l) => {
+      let listItem = document.createElement("li");
+      listItem.innerHTML = l.phrase;
+      list.appendChild(listItem);
+    });
+    funcTitleDiv.appendChild(funcTitle);
+    funcTitleDiv.appendChild(icon);
+    phrasesContent.appendChild(list);
+    phrases.appendChild(phrasesContent);
+    func.appendChild(funcTitleDiv);
+    func.appendChild(phrases);
+    functionsDiv.appendChild(func);
+  });
+  let titles = document.querySelectorAll(".title");
+  titles.forEach((title) => {
+    title.onclick = function () {
+      if (title.classList.contains("active")) {
+        title.classList.remove("active");
+        title.nextElementSibling.style.height = 0;
+      } else {
+        title.classList.add("active");
+        title.nextElementSibling.style.height =
+          title.nextElementSibling.scrollHeight + "px";
+      }
+    };
+  });
+}
+displayElementsFrom(functions);
